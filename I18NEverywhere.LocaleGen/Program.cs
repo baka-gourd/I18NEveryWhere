11202ab992379a9527e835eabd3b3e338279c6cf
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2024-03-25 19:01:39 +0800

    feat(I18NEverywhere.Localegen): add project

---
+++ I18NEverywhere.LocaleGen/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+using Colossal;
+
+namespace I18NEverywhere.LocaleGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var setting = new Setting(new I18NEverywhere());
+            var locale = new LocaleEN(setting);
+            var e = new Dictionary<string, string>(
+                locale.ReadEntries([], []));
+            var str = JsonSerializer.Serialize(e, new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            });
+
+            Console.WriteLine(str);
+        }
+    }
+}
